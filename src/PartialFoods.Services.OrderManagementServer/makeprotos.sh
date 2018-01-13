dc824920e3e3826d96e4497c6f82d020f03aa8a6
autodidaddict <alothien@gmail.com>
2018-01-13 14:33:17 -0500

    Changing default port to 8081

--- src/PartialFoods.Services.OrderManagementServer/makeprotos.sh
+++ src/PartialFoods.Services.OrderManagementServer/makeprotos.sh
@@ -1,4 +1,4 @@
 PROJDIR=`pwd`
-cd ~/.nuget/packages/grpc.tools/1.6.1/tools/linux_x64
+cd ~/.nuget/packages/grpc.tools/1.8.3/tools/linux_x64
 protoc -I $PROJDIR/../../proto --csharp_out $PROJDIR/RPC --grpc_out $PROJDIR/RPC $PROJDIR/../../proto/ordermgmt.proto $PROJDIR/../../proto/partialfoods.proto --plugin=protoc-gen-grpc=grpc_csharp_plugin
 cd -
